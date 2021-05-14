9fcf50025360bca404419f1f9b437b34a7ae9f89
Tyler <tylercole8899@gmail.com>
2021-05-14 10:54:10 -0400

    Fetch location, 5 day, and hourly forecast. Display 5 day

---
+++ src/components/WeatherCard/cardData.ts
@@ -0,0 +1,11 @@
+export const CardData = {
+    detailedForecast: "Mostly cloudy, with a low around 47. West wind around 7 mph.",
+    isDaytime: false,
+    name: "Tonight",
+    number: 2,
+    shortForecast: "Mostly Cloudy",
+    temperature: 47,
+    temperatureUnit: "F",
+    windDirection: "W",
+    windSpeed: "7 mph"
+}
